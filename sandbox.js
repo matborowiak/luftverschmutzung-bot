0b25f002de3d8a4478f1b2bb3f52e3e9e068edcc
Mateusz Borowiak <m.borowiak@hotmail.com>
2020-01-05 15:57:06 +0100

    refactoring, settings, lambda working

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,6 @@
-// todo: add more conditions for different types of pollutants data
-// todo: add hashtags
-// messages like: Good morning Berlin! , Good afternoon Poznań!, Good night Hamburg! etc. #smaczki
-
 const Twit = require('twit')
 const axios = require('axios')
-const token = process.env //require('./_keys/API_KEYS')
+const token = require('./_keys/API_KEYS')
 
 // twit config for twitter api auth
 const t = new Twit({
@@ -121,45 +117,3 @@ exports.handler = async event => {
   })
   return asyncStatusHanlder
 }
-
-
-// DUMMY STATUS UPDATE
-// Berlin, Germany - air quality is considered satisfactory, and air pollution poses little or no risk.
-
-// Air Quality Index - 20 | Good 💚
-// -----------------
-// PM₂₅ - 1027.6
-// PM₁₀ - 20
-// O₃ - 2.9
-
-// DUMMY POLLUTION DATA TREE
-// {
-//   "status": "ok",
-//   "data": {
-//     "aqi": 29,
-//     "idx": 6132,
-//     "attributions": [
-//       {
-//         "url": "http://www.stadtentwicklung.berlin.de/umwelt/luftqualitaet/",
-//         "name": "Berlin Air Quality - (Luftqualität in Berlin)"
-//       },
-//       { "url": "https://waqi.info/", "name": "World Air Quality Index Project" }
-//     ],
-//     "city": {
-//       "geo": [52.5200066, 13.404954],
-//       "name": "Berlin, Germany",
-//       "url": "https://aqicn.org/city/germany/berlin"
-//     },
-//     "dominentpol": "pm10",
-//     "iaqi": {
-//       "no2": { "v": 23.4 },
-//       "o3": { "v": 2.9 },
-//       "p": { "v": 990.9 },
-//       "pm10": { "v": 29 },
-//       "t": { "v": 4.4 },
-//       "w": { "v": 7.5 }
-//     },
-//     "time": { "s": "2019-12-22 19:00:00", "tz": "+01:00", "v": 1577041200 },
-//     "debug": { "sync": "2019-12-23T03:37:46+09:00" }
-//   }
-// }
---
+++ testSandbox.js
@@ -0,0 +1,14 @@
+const lambdaFunction = require('./sandbox.js')
+
+//Call your exports function with required params
+//In AWS lambda these are event, content, and callback
+//event and content are JSON object and callback is a function
+//In my example i'm using empty JSON
+lambdaFunction.handler(
+  {}, //event
+  {}, //content
+  (data, ss) => {
+    //callback function with two arguments
+    console.log('testHandler Data: ', data)
+  }
+)
